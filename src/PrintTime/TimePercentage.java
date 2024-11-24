8d858e2b5fc07d0292f1782de1fee27b20a2f511
Ricardo <kalamarikaka@gmail.com>
2024-11-24 18:39:48 +0100

    status bar fix

--- src/PrintTime/TimePercentage.java
+++ src/PrintTime/TimePercentage.java
@@ -81,7 +81,7 @@ public class TimePercentage {
     protected static void printStatusBar(double hour) {
         StringBuilder sb = new StringBuilder();
         int widthOfPrintedText = 24; // MAX 24 !!! TODO
-        double maxStatusBarLength = Math.nextDown(((int)hour)/(24/widthOfPrintedText));
+        double maxStatusBarLength = Math.round(((int)hour)/(24/widthOfPrintedText));
         for(int i = 0; i <= maxStatusBarLength; i++) {
             sb.append("\u2588");
         }
